81a53dad42efa771daf514f95f5a47210ab8e2eb
Loic DUMAY <dumay.famille@gmail.com>
2021-03-30 18:43:01 +0200

    v0.1

---
+++ index.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//FR" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+    <head>
+        <!-- Title -->
+        <title>LDumay.Fr</title>
+        <!-- ./Title -->
+
+        <!-- MetasDatas -->
+        <meta name="Content-Type" content="UTF-8" charset="UTF-8">
+        <meta name="Content-Language" content="fr">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="Description" content="Bonjour et Bienvenue à vous ! Je suis heureux de vous voir sur mon site Web personnel. Celui-ci regroupe tout ce dont vous avez besoin de savoir sur moi-même. Bonne visite.<br/>Pour plus de description, n'héistez pas à consulter directement mon site web https://ldumay.fr/. A bientôt !">
+        <meta name="Subject" content="Site Vitrine Personnel">
+        <meta name="Copyright" content="Loïc Dumay (alias LDumay)">
+        <meta name="Author" content="Loïc Dumay (alias LDumay)">
+        <meta name="Publisher" content="Loïc Dumay (alias LDumay)">
+        <meta name="Identifier-Url" content="https://ldumay.fr">
+        <meta name="Reply-To" content="hello@ldumay.fr">
+        <meta name="Revisit-After" content="10 days">
+        <meta name="Robots" content="index">
+        <meta name="Rating" content="general">
+        <meta name="Distribution" content="global">
+        <meta name="Geography" content="Ile-De-France, France">
+        <meta name="Category" content="internet">
+        <!-- ./MetasDatas -->
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="https://dev.ldumay.fr/resources/img/favicons/favicon.png">
+        <!-- ./Favicon -->
+
+        <!-- Apple Icon -->
+        <link rel="apple-touch-icon" href="https://dev.ldumay.fr/resources/img/favicons/favicon_apple.png">
+        <!-- ./Apple Icon -->
+
+        <!-- Icons -->
+        <link rel="stylesheet" type="text/css" href="https://glyphsearch.com/bower_components/font-awesome/css/all.min.css"/>
+
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/font-awesome/font-awesome.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/foundation-icons/foundation-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/ico-moon/ico-moon.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/ion-icons/ion-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/material-design-icons/material-design-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/oct-icons/oct-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/pe7-stroke-icons/pe-icon-7-stroke.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/simple-line-icons/simple-line-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/themify-icons/themify-icons.css"/>
+        <!-- ./Icons -->
+
+        <!-- Bootstrap core CSS -->
+        <link href="https://dev.ldumay.fr/resources/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://dev.ldumay.fr/resources/bootstrap/4.1.3/css/bootstrap-grid.min.css" rel="stylesheet">
+
+        <!-- GoogleFonts -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poiret+One&family=Alatsi&family=Roboto&family=Righteous">
+        <!-- ./GoogleFonts -->
+
+        <!-- MyCSS -->
+        <link href="styles.css" rel="stylesheet">
+        <!-- ./MyCSS -->
+    </head>
+    
+    <!-- Body -->
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <h1>LearnEnglish</h1>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <h4>Content</h4>
+                    
+                    <hr>
+                    <p>© Copyright 2020 - Créer par <a target="_blank" href="https://ldumay.fr/">Loïc Dumay</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+    <!-- ./Body -->
+
+    <!-- Action_Page -->
+    <!--
+    <div class="action_page">
+        <img class="haut" src="img/action_page/haut.png">
+        <img class="bas" src="img/action_page/bas.png">
+    </div>
+    -->
+    <!-- ./Action_Page -->
+
+    <!-- Javascript -->
+        <!-- Javascript_And_JQuery_Importations -->
+        <script type="text/javascript" src="index.js"></script>
+        <!-- ./Javascript_And_JQuery_Importations -->
+    <!-- ./Javacsript -->
+</html>
